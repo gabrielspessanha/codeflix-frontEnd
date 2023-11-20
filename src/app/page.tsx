1bc8571fb216ea7a8d8d1357d65e09148485b167
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-11-20 16:58:27 -0300

    criação dos elementos visuais e também da sua responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head'
 import styles from '../styles/HomeNoAuth.module.scss'
 import HeaderNoAuth from '../components/homeNoAuth/headerNoAuth'
+import PresentationSection from '@/components/homeNoAuth/presentationSection'
 
 const HomeNoAuth = ()=>{
   return (
     <>
       <main>
-        <HeaderNoAuth />
+        <div className={styles.sectionBackground}>
+          <HeaderNoAuth />
+          <PresentationSection />
+        </div>
       </main>
     </>
   )
