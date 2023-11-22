9f3b4e5ffb7d64e8b072837f140eca42d3bb2130
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-11-22 09:36:39 -0300

    Criação dos elementos do visual e também da responsividade dos cards - Seção de cards homeNoAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/HomeNoAuth.module.scss'
 import HeaderNoAuth from '../components/homeNoAuth/headerNoAuth'
 import PresentationSection from '@/components/homeNoAuth/presentationSection'
+import CardsSection from '@/components/homeNoAuth/cardsSection'
 
 const HomeNoAuth = ()=>{
   return (
@@ -11,6 +12,7 @@ const HomeNoAuth = ()=>{
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <CardsSection />
       </main>
     </>
   )
