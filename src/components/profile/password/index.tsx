523d725e23e7917d226f02ccff21e57bdd74da72
gabrielspessanha <gabrielpessanha2g@gmail.com>
2023-12-03 11:39:21 -0300

    Conexão do backend em relação ao update de senha, e criação do método e toda configuração de conexão com o front

--- src/components/profile/password/index.tsx
+++ src/components/profile/password/index.tsx
@@ -1,10 +1,82 @@
 'use client'
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap'
 import styles from '../../../styles/profile.module.scss';
+import { FormEvent, useEffect, useState } from 'react';
+import profileService from '@/services/profileService';
+import ToastComponent from '@/components/common/toast';
 
 const Password = () => {
+  const [currentPassword, setCurrentPassword] = useState("")
+  const [newPassword, setNewPassword] = useState("")
+  const [confirmNewPassword, setConfirmNewPassword] = useState("")
+
+  const [color, setColor] = useState("")
+  const [toastIsOpen, setToastIsOpen] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+
+
+
+  useEffect(()=>{
+    profileService.fetchCurrent().then((password) => {
+      setCurrentPassword(password.currentPassword)
+      setNewPassword(password.newPassword)
+    })
+  },[])
+
+  const handlePasswordUpdate = async function(event: FormEvent){
+    event.preventDefault()
+    if(newPassword !== confirmNewPassword){
+      setToastIsOpen(true)
+      setErrorMessage("Senha e confirmação de senha estão diferentes")
+      setColor("bg-danger")
+      setTimeout(()=>{
+        setToastIsOpen(false)
+      }, 1000 * 3);
+
+      return
+    }
+
+    if (currentPassword === newPassword){
+      setToastIsOpen(true)
+      setErrorMessage("Nova senha e antiga senha são iguais")
+      setColor("bg-danger")
+      setTimeout(()=>{
+        setToastIsOpen(false)
+      }, 1000 * 3);
+
+      return
+    }
+
+    const res = await profileService.passwordUpdate({
+      currentPassword,
+      newPassword
+    });
+
+    if(res === 204){
+      setToastIsOpen(true)
+      setErrorMessage("Senha alterada com sucesso!")
+      setColor("bg-success")
+      setTimeout(()=>{
+        setToastIsOpen(false)
+      }, 1000 * 3);
+
+      setCurrentPassword("")
+      setNewPassword("")
+      setConfirmNewPassword("")
+    }
+
+    if(res === 400){
+      setToastIsOpen(true)
+      setErrorMessage("Senha atual incorreta!")
+      setColor("bg-danger")
+      setTimeout(()=>{
+        setToastIsOpen(false)
+      }, 1000 * 3);
+    }
+  }
   return (
-    <Form className={styles.form}>
+    <>
+    <Form onSubmit={handlePasswordUpdate} className={styles.form}>
       <div className={styles.inputNormalDiv}>
         <FormGroup>
           <Label className={styles.label} for="currentPassword">SENHA ATUAL</Label>
@@ -16,6 +88,8 @@ const Password = () => {
             required
             minLength={6}
             maxLength={12}
+            value={currentPassword}
+            onChange={(event)=> setCurrentPassword(event.currentTarget.value)}
             className={styles.input}
           />
         </FormGroup>
@@ -32,6 +106,8 @@ const Password = () => {
             required
             minLength={6}
             maxLength={12}
+            value={newPassword}
+            onChange={(event)=> setNewPassword(event.currentTarget.value)}
             className={styles.inputFlex}
           />
         </FormGroup>
@@ -46,6 +122,8 @@ const Password = () => {
             required
             minLength={6}
             maxLength={12}
+            value={confirmNewPassword}
+            onChange={(event)=> setConfirmNewPassword(event.currentTarget.value)}
             className={styles.inputFlex}
           />
         </FormGroup>
@@ -54,6 +132,8 @@ const Password = () => {
           Salvar Alterações
         </Button>
     </Form>
+    <ToastComponent color={color} isOpen={toastIsOpen} message={errorMessage} />
+    </>
   )
 }
 
--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -7,6 +7,10 @@ interface UserParams {
   email: string
   created_at: string
 }
+interface PasswordParams{
+  currentPassword: string
+  newPassword: string
+}
 
 const profileService = {
   fetchCurrent: async ()=> {
@@ -37,6 +41,23 @@ const profileService = {
 
     return res.status
   },
+  passwordUpdate: async (params: PasswordParams)=>{
+    const token = sessionStorage.getItem('codeflix-token')
+
+    const res = await api.put('/users/current/password', params, {
+      headers:{
+        Authorization: `Bearer ${token}`
+      }
+    }).catch(error => {
+      if (error.response.status === 400 || error.response.status === 401) {
+        return error.response
+      }
+
+      return error
+    })
+
+    return res.status
+  }
 }
 
 export default profileService
